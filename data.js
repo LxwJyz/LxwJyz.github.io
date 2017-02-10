a1edcc5f439fe4f89cbbbab29d4c40a8e77308e5
lxw <120577229@qq.com>
2017-02-10 10:21:28 +0800

    添加index.js

---
+++ data.js
@@ -0,0 +1,212 @@
+var arr = [
+				{
+					// 小米手机
+					data:[
+						{
+							imgsrc:"images/0-0.jpeg",
+							name:"小米Note 2",
+							price:"2799元起"
+						},
+						{
+							imgsrc:"images/0-1.jpeg",
+							name:"小米MIX",
+							price:"3499元"
+						},
+						{
+							imgsrc:"images/0-2.jpeg",
+							name:"小米5s",
+							price:"1899元"
+						},
+						{
+							imgsrc:"images/0-3.jpeg",
+							name:"小米5s Plus",
+							price:"2299元起"
+						},
+						{
+							imgsrc:"images/0-4.jpeg",
+							name:"小米手机5",
+							price:"1599元起"
+						},
+						{
+							imgsrc:"images/0-5.jpeg",
+							name:"小米Max",
+							price:"1299元起"
+						}
+					]
+				},
+				{
+					// 红米
+					data:[
+						{
+							imgsrc:"images/1-0.jpeg",
+							name:"红米Note 4",
+							price:"999元起"
+						},
+						{
+							imgsrc:"images/1-1.jpeg",
+							name:"红米4",
+							price:"799元起"
+						}
+					]
+				},
+				{
+					// 平板.笔记本
+					data:[
+						{
+							imgsrc:"images/2-0.jpeg",
+							name:"小米平板2 16GB",
+							price:"999元"
+						},
+						{
+							imgsrc:"images/2-1.jpeg",
+							name:"小米平板2 64GB",
+							price:"1299元"
+						},
+						{
+							imgsrc:"images/2-2.jpeg",
+							name:"小米平板2 64GB Windows版",
+							price:"1299元"
+						},
+						{
+							imgsrc:"images/2-3.jpeg",
+							name:"小米笔记本Air 12.5",
+							price:"3499元"
+						},
+						{
+							imgsrc:"images/2-4.jpeg",
+							name:"小米笔记本Air 13.3",
+							price:"4999元"
+						}
+					]
+				},
+				{
+					// 电视
+					data:[
+						{
+							imgsrc:"images/3-0.png",
+							name:"小米电视3s 48英寸",
+							price:"2299元"
+						},
+						{
+							imgsrc:"images/3-1.png",
+							name:"小米电视3s 55英寸",
+							price:"3699元起"
+						},
+						{
+							imgsrc:"images/3-2.png",
+							name:"小米电视3s 60英寸",
+							price:"4499元"
+						},
+						{
+							imgsrc:"images/3-3.png",
+							name:"小米电视3s 65英寸",
+							price:"5999元"
+						},
+						{
+							imgsrc:"images/3-4.png",
+							name:"小米电视3 70英寸",
+							price:"9999元"
+						},
+						{
+							imgsrc:"images/3-5.png",
+							name:"查看全部",
+							price:"小米电视"
+						}
+					]
+				},
+				{
+					// 盒子.影音
+					data:[
+						{
+							imgsrc:"images/4-0.png",
+							name:"小米盒子3s",
+							price:"299元"
+						},
+						{
+							imgsrc:"images/4-1.png",
+							name:"小米盒子3c",
+							price:"199元"
+						},
+						{
+							imgsrc:"images/4-2.jpeg",
+							name:"小米盒子3 增强版",
+							price:"399元"
+						},
+						{
+							imgsrc:"images/4-3.png",
+							name:"小米盒子mini版",
+							price:"179元"
+						},
+						{
+							imgsrc:"images/4-4.png",
+							name:"小米家庭影院",
+							price:"1999元"
+						},
+						{
+							imgsrc:"images/4-5.jpeg",
+							name:"小米家庭音响 标准版",
+							price:"699元"
+						}
+					]
+				},
+				{
+					// 路由器
+					data:[
+						{
+							imgsrc:"images/5-0.jpeg",
+							name:"全新小米路由器",
+							price:"699元"
+						},
+						{
+							imgsrc:"images/5-1.jpeg",
+							name:"小米路由器 3",
+							price:"149元"
+						},
+						{
+							imgsrc:"images/5-2.jpeg",
+							name:"小米路由器 3C",
+							price:"99元"
+						},
+						{
+							imgsrc:"images/5-3.jpeg",
+							name:"小米WiFi放大器 2",
+							price:"49元"
+						}
+					]
+				},
+				{
+					// 智能硬件
+					data:[
+						{
+							imgsrc:"images/6-0.jpeg",
+							name:"九号平衡车",
+							price:"1999元"
+						},
+						{
+							imgsrc:"images/6-1.jpeg",
+							name:"小米净水器",
+							price:"1299元起"
+						},
+						{
+							imgsrc:"images/6-2.jpeg",
+							name:"米家压力IH电饭煲",
+							price:"999元"
+						},
+						{
+							imgsrc:"images/6-3.jpeg",
+							name:"小米空气净化器 2",
+							price:"699元"
+						},
+						{
+							imgsrc:"images/6-4.jpeg",
+							name:"智能摄像机",
+							price:"399元"
+						},
+						{
+							imgsrc:"images/6-5.jpeg",
+							name:"查看全部",
+							price:"智能硬件"
+						}
+					]
+				}
+			];
